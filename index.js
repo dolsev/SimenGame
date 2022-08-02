3c604a8c317b2701e539cca0f057a68fbce0aaf1
Vlad Kel <108391008+dolsev@users.noreply.github.com>
2022-08-02 13:09:07 +0300

    Update index.js

--- index.js
+++ index.js
@@ -58,3 +58,11 @@ function checkAnswer (currentLevel){
         
     }
 }
+
+$(document).on('mouseenter', '.divbutton', function () {
+    $(this).find(":button").show();
+}).on('mouseleave', '.divbutton', function () {
+    $(this).find(":button").hide();
+}).on('click', ':button', function() {
+    $(this).parent().remove();
+});
